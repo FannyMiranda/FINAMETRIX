39cf15071e35d2cb1871c974a0c44f4f5ea0ef7e
Fanny Miranda <fannymirandaalemany.com>
2018-07-17 18:06:29 +0200

    principio

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+{
+                res.render('home',{
+                    title: 'Home',
+                    layout: '../views/index.hbs',
+                    destinos: destinos
+                });
+            }
+        })
+    
+
+module.exports = router;
